93b2f99b0d4dfb9ed60079f6fc12f16ee0bdb219
lshep <lori.shepherd@roswellpark.org>
2024-04-12 07:43:23 -0400

    Undeprecate

--- R/zzz.R
+++ R/zzz.R
@@ -10,10 +10,3 @@
     }
     invisible()
 }
-.onAttach <- function(libname, pkgname) {
-    msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.20")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
-}
-
